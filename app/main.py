38502771ba929fce5793b47954d2ab33417a0d89
Sierra Welsch <sierrasathi4@gmail.com>
2026-04-11 22:31:26 -0400

    added comment about how to run streamlit app

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ import sys
 import os
 import pydeck as pdk
 
+# TO RUN: streamlit run main.py
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.models.lstm import ReadyLSTM
 
